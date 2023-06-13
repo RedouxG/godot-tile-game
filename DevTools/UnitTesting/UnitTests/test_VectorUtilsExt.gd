a7eb05acba562f5d8c91de8e29caacf246b18e39
RedouxG <chlebojad003@gmail.com>
2023-06-13 22:54:04 +0200

    Refactor

--- DevTools/UnitTesting/UnitTests/test_VectorUtilsExt.gd
+++ DevTools/UnitTesting/UnitTests/test_VectorUtilsExt.gd
@@ -14,51 +14,66 @@ var Rng := RandomNumberGenerator.new()
 # Functions
 ### ----------------------------------------------------
 
+func do_test_for_v2_and_v2i(function:Callable, given:Array[Array], expected:Variant) -> void:
+	assert_eq(function.callv(given[0]), expected,
+			function.get_method() + " failed for Vector2i")
+	assert_eq(function.callv(given[1]), expected,
+			function.get_method() + " failed for Vector2")
+
 func test_vector_conversion() -> void:
-	for i in range(10):
+	for i in range(4):
 		var x:int = Rng.randi_range(-10_000_000, 10_000_000)
 		var y:int = Rng.randi_range(-10_000_000, 10_000_000)
 		var z:int = Rng.randi_range(-10_000_000, 10_000_000)
-		assert_eq(VectorUtilsExt.vec2i_vec3i(Vector2i(x,y), z), Vector3i(x,y,z),
-			"vec2i_vec3i failed for Vector2i")
-		assert_eq(VectorUtilsExt.vec2i_vec3i(Vector2(x,y), z), Vector3i(x,y,z),
-			"vec2i_vec3i failed for Vector2")
-		
-		assert_eq(VectorUtilsExt.vec3i_vec2i(Vector3i(x,y,z)), Vector2i(x,y),
-			"vec3i_vec2i failed for Vector3i")
-		assert_eq(VectorUtilsExt.vec3i_vec2i(Vector3(x,y,z)), Vector2i(x,y),
-			"vec3i_vec2i failed for Vector3")
+		do_test_for_v2_and_v2i(
+			Callable(VectorUtilsExt, "vec2i_vec3i"),
+			[[Vector2i(x,y), z], [Vector2(x,y), z]], 
+			Vector3i(x,y,z)
+		)
+
+		do_test_for_v2_and_v2i(
+			Callable(VectorUtilsExt, "vec3i_vec2i"),
+			[[Vector3i(x,y,z)], [Vector3(x,y,z)]], 
+			Vector2i(x,y)
+		)
 
 func test_vector_scale() -> void:
+	var SCALE = GLOBAL.MAP.CHUNK_SIZE
 	for i in range(-3,3):
-		assert_eq(VectorUtilsExt.scale_down_vec2i(Vector2i(i*16,i*16), 16), Vector2i(i,i),
-			"scale_down_vec2i failed for Vector2i")
-		assert_eq(VectorUtilsExt.scale_down_vec2i(Vector2(i*16,i*16), 16), Vector2i(i,i),
-			"scale_down_vec2i failed for Vector2")
-		
-		assert_eq(VectorUtilsExt.scale_down_vec3i(Vector3i(i*16,i*16,i*16), 16), Vector3i(i,i,i),
-			"scale_down_vec3i failed for Vector3i")
-		assert_eq(VectorUtilsExt.scale_down_vec3i(Vector3(i*16,i*16,i*16), 16), Vector3i(i,i,i),
-			"scale_down_vec3i failed for Vector3")
-		
-		assert_eq(VectorUtilsExt.scale_down_vec3i_no_z(Vector3i(i*16,i*16,i*16), 16), Vector3i(i,i,i*16),
-			"scale_down_vec3i_no_z failed for Vector3i")
-		assert_eq(VectorUtilsExt.scale_down_vec3i_no_z(Vector3(i*16,i*16,i*16), 16), Vector3i(i,i,i*16),
-			"scale_down_vec3i_no_z failed for Vector3")
+		do_test_for_v2_and_v2i(
+			Callable(VectorUtilsExt, "scale_down_vec2i"),
+			[[Vector2i(i*SCALE,i*SCALE), SCALE], [Vector2(i*SCALE,i*SCALE), SCALE]], 
+			Vector2i(i,i)
+		)
+
+		do_test_for_v2_and_v2i(
+			Callable(VectorUtilsExt, "scale_down_vec3i"),
+			[[Vector3i(i*SCALE,i*SCALE,i*SCALE), SCALE], [Vector3(i*SCALE,i*SCALE,i*SCALE), SCALE]], 
+			Vector3i(i,i,i)
+		)
+
+		do_test_for_v2_and_v2i(
+			Callable(VectorUtilsExt, "scale_down_vec3i_no_z"),
+			[[Vector3i(i*SCALE,i*SCALE,i*SCALE), SCALE], [Vector3(i*SCALE,i*SCALE,i*SCALE), SCALE]], 
+			Vector3i(i,i,i*SCALE)
+		)
 
 func test_vector_chunk() -> void:
-	var TEMPLATE_MINUS_VEC2:Array[Vector2i] = [
+	var TEMPLATE_VEC2:Array[Vector2i] = [
 		Vector2i(-2,-2), Vector2i(-2,-1), Vector2i(-1,-2), Vector2i(-1,-1)]
-	assert_eq(VectorUtilsExt.vec2i_get_positions_in_chunk(Vector2i(-1,-1), 2), TEMPLATE_MINUS_VEC2,
-		"vec2i_get_positions_in_chunk failed for Vector2i")
-	assert_eq(VectorUtilsExt.vec2i_get_positions_in_chunk(Vector2i(-1,-1), 2), TEMPLATE_MINUS_VEC2,
-		"vec2i_get_positions_in_chunk failed for Vector2")
-	var TEMPLATE_MINUS_VEC3_NO_Z:Array[Vector3i] = [
+	do_test_for_v2_and_v2i(
+		Callable(VectorUtilsExt, "vec2i_get_positions_in_chunk"),
+		[[Vector2i(-1,-1), 2], [Vector2(-1,-1), 2]], 
+		TEMPLATE_VEC2
+	)
+
+	var TEMPLATE_VEC3_NO_Z:Array[Vector3i] = [
 		Vector3i(-2,-2, 0), Vector3i(-2,-1, 0), Vector3i(-1,-2, 0), Vector3i(-1,-1, 0)]
-	assert_eq(VectorUtilsExt.vec3i_get_positions_in_chunk_no_z(Vector3i(-1,-1, 0), 2), TEMPLATE_MINUS_VEC3_NO_Z,
-		"vec2i_get_positions_in_chunk failed for Vector3i")
-	assert_eq(VectorUtilsExt.vec3i_get_positions_in_chunk_no_z(Vector3(-1,-1, 0), 2), TEMPLATE_MINUS_VEC3_NO_Z,
-		"vec2i_get_positions_in_chunk failed for Vector3")
+	do_test_for_v2_and_v2i(
+		Callable(VectorUtilsExt, "vec3i_get_positions_in_chunk_no_z"),
+		[[Vector3i(-1,-1, 0), 2], [Vector3(-1,-1, 0), 2]], 
+		TEMPLATE_VEC3_NO_Z
+	)
 
 func test_vector_range() ->void:
 	var TEMPLATE_VEC2:Array[Vector2i] = [
@@ -66,7 +81,8 @@ func test_vector_range() ->void:
 		Vector2i(0, -1),  Vector2i(0, 0),  Vector2i(0, 1), 
 		Vector2i(1, -1),  Vector2i(1, 0),  Vector2i(1, 1)]
 	
-	assert_eq(VectorUtilsExt.vec2i_get_positions_in_range(Vector2i(0,0), 1), TEMPLATE_VEC2,
-		"vec2i_get_positions_in_range failed for Vector2i")
-	assert_eq(VectorUtilsExt.vec2i_get_positions_in_range(Vector2(0,0), 1), TEMPLATE_VEC2,
-		"vec2i_get_positions_in_range failed for Vector2")
+	do_test_for_v2_and_v2i(
+		Callable(VectorUtilsExt, "vec2i_get_positions_in_range"),
+		[[Vector2i(0,0), 1], [Vector2(0,0), 1]], 
+		TEMPLATE_VEC2
+	)
